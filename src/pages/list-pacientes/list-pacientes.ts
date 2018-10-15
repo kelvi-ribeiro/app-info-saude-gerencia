1d6f32b4a2b14de54b3ef0a21131b798783462eb
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-15 17:30:10 -0300

    Comecando logica e layout para trocar as linhas de cuidado

--- src/pages/list-pacientes/list-pacientes.ts
+++ src/pages/list-pacientes/list-pacientes.ts
@@ -1,14 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { PacienteService } from '../../services/domain/paciente.service';
 import { NotificacoesService } from '../../services/domain/notificacoes.service';
-
-/**
- * Generated class for the ListPacientesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { LinhaCuidadoService } from '../../services/domain/linha.cuidado';
 
 @IonicPage()
 @Component({
@@ -21,32 +15,55 @@ export class ListPacientesPage {
   pages;
   pageAtual = 0;
   totalPages;  
+  linhasCuidado = [];
+  linhaCuidadoId;
+  @ViewChild('slidesLinhasCuidado') slidesLinhasCuidado: Slides;
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private pacienteService:PacienteService,
+    private linhaCuidadoService:LinhaCuidadoService,
     private notificacoesService:NotificacoesService) {
   }
 
   ionViewDidLoad() {    
-    this.findPacientes()
+    this.findPacientes()  
+    this.findLinhasCuidado()  
+  }
+  slideChanged(){
+    if(this.slidesLinhasCuidado.getActiveIndex() === 0){
+      this.linhaCuidadoId = ''
+    }
+    
+    if(this.slidesLinhasCuidado.getActiveIndex()  > this.linhasCuidado.length){
+      return
+    }
+    this.linhaCuidadoId = this.slidesLinhasCuidado.getActiveIndex() 
+    this.findPacientes(this.pacienteNome,this.linhaCuidadoId,this.pageAtual) 
+  }
+  findLinhasCuidado(){
+    this.linhaCuidadoService.findAll()
+    .then(res=>{
+      this.linhasCuidado = res
+      this.linhasCuidado.unshift({id:0,nome:'Todas'});
+    })
   }
   changePage(clickedPage){
   if(this.pageAtual === clickedPage) return 
   this.pageAtual = clickedPage
-  this.findPacientes(this.pacienteNome,null,this.pageAtual)
+  this.findPacientes(this.pacienteNome,this.linhaCuidadoId,this.pageAtual)
   }
 
   nextPage(){
     if(this.pageAtual === this.totalPages) return
     this.pageAtual++;
-    this.findPacientes(this.pacienteNome,null,this.pageAtual)
+    this.findPacientes(this.pacienteNome,this.linhaCuidadoId,this.pageAtual)
   }
   previousPage(){
     if(this.pageAtual === 0) return
     this.pageAtual--;
     
-    this.findPacientes(this.pacienteNome,null,this.pageAtual)
+    this.findPacientes(this.pacienteNome,this.linhaCuidadoId,this.pageAtual)
   }
   openActions(paciente){
     paciente.actionOpened =  paciente.actionOpened ? false : true

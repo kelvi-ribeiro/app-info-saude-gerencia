4463985abc9c01665cda105a7959318fcde3d7d0
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-22 10:40:15 -0300

    Salvando data de nascimento com sucesso

--- src/components/form-dados-pessoais/form-dados-pessoais.ts
+++ src/components/form-dados-pessoais/form-dados-pessoais.ts
@@ -18,17 +18,25 @@ export class FormDadosPessoaisComponent {
   constructor(private events:Events) { 
   }
   
-  onChange(field,value){
-    if(field === 'dataNascimento'){
-      value = new Date(value)
-    }
-    this.events.publish('editar-dados-pessoa:paciente',field,value)      
+  onChange(field,value){    
+    if(field === 'dataNascimento'){           
+      if (Object.prototype.toString.call(new Date(value)) === "[object Date]") {        
+        if (isNaN(new Date(value).getTime())) {  // d.valueOf() could also work
+          return
+        } else {                
+            value = new Date(value).setDate(new Date(value).getDate() +1)             
+            this.events.publish('editar-dados-pessoa:paciente',field,value)            
+        }
+      } else {
+        return
+      }      
+    }else{
+      this.events.publish('editar-dados-pessoa:paciente',field,value)            
+
+    }    
   }
   convertTimeStampToDate(timestamp){
     const dataTratada = new Date(timestamp)
     return `${dataTratada.getFullYear()}-${dataTratada.getMonth() + 1 >= 10 ? '':0}${dataTratada.getMonth() + 1}-${dataTratada.getDate() >= 10 ? '':0}${dataTratada.getDate()}`
   }
-  
-  
-
 }

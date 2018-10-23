8bb69cddb94e4a2e98934973cdb0ce0b7533388f
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-23 05:33:18 -0300

    Criando servico de pacienteLinhaCuidado e colocando no importando como service em com component.module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -8,6 +8,7 @@ import { DadosContatoComponent } from './dados-contato/dados-contato';
 import { SideMenuComponent } from './side-menu/side-menu';
 import { FormDadosPessoaisComponent } from './form-dados-pessoais/form-dados-pessoais';
 import { NaturalidadeService } from '../services/domain/naturalidade.service';
+import { PacienteLinhaCuidadoService } from '../services/domain/paciente.linha.cuidado.service';
 @NgModule({
 	declarations: [DadosPessoaisComponent,
     DadosMedicosComponent,
@@ -23,7 +24,8 @@ import { NaturalidadeService } from '../services/domain/naturalidade.service';
     SideMenuComponent,
     FormDadosPessoaisComponent],
     providers:[
-        NaturalidadeService
+        NaturalidadeService,
+        PacienteLinhaCuidadoService
     ]
 })
 export class ComponentsModule {}
---
+++ src/services/domain/paciente.linha.cuidado.service.ts
@@ -0,0 +1,40 @@
+import { HandlerResponseProvider } from './../handler-response/handler-response';
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../../config/api.config";
+import { StorageService } from "../storage.service";
+import { Headers } from '@angular/http';
+
+
+
+
+@Injectable()
+export class PacienteLinhaCuidadoService {
+  constructor(    
+    public storage: StorageService,
+    public handlerResponseService: HandlerResponseProvider
+
+  ) {
+  }
+
+  findAllByPacienteId(pacienteId:number) {
+    let headers = new Headers();
+    return this.storage.getUserCredentials()
+      .then(userCredentials => {
+        if (!userCredentials) {
+          return;
+        }
+        headers.append('Authorization', `Bearer ${userCredentials['token']}`)
+        return this.handlerResponseService.handlerResponse(
+          "get",
+          `${API_CONFIG.baseUrl}/pacientes-linhas-cuidado/paciente-id/${pacienteId}`,
+          null,
+          headers
+        );
+      })
+  }
+
+}
+
+
+
+

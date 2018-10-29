7c72feca62f225c6c1e563577e28d18628df47e5
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-29 04:55:01 -0300

    Quase finalizando form de dados contato

--- src/components/form-dados-contato/form-dados-contato.ts
+++ src/components/form-dados-contato/form-dados-contato.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { TelefoneService } from '../../services/domain/telefone.service';
+import { NotificacoesService } from '../../services/domain/notificacoes.service';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the FormContatoComponent component.
@@ -13,10 +16,47 @@ import { Component, Input } from '@angular/core';
 export class FormDadosContatoComponent {
 
   @Input() paciente;
+  telefones: any;
 
-  constructor() {
-    
-    
+  constructor(
+    private telefoneService:TelefoneService,
+    private notificacoesService:NotificacoesService,
+    private alertCtrl:AlertController) {
+    setTimeout(() => {
+      this.findAllByPessoaId()      
+    }, 50)    
   }
-
+  findAllByPessoaId(){
+    this.telefoneService.findAllByPessoaId(this.paciente.pessoa.id)
+    .then(res=>{
+      this.telefones = res
+    })
+    .catch(()=>{
+      this.notificacoesService.presentAlertErro()
+    })
+  }
+  alertRemoverTelefone(pessoaId){      
+    let alert = this.alertCtrl.create({
+      title:'Tem certeza ?',
+      buttons: [
+        {
+          text: 'Sim',            
+          handler: () => {
+            this.telefoneService.delete(pessoaId)
+            .then(() => {                
+             this.findAllByPessoaId()                 
+              this.notificacoesService.presentToast('Telefone removido',null,2000,'top')
+            }).catch(()=>{
+              this.notificacoesService.presentAlertErro();
+            })
+          }
+        },
+        {
+          text: 'Não',             
+        },
+     
+         ]
+    });
+    alert.present()
+  }    
 }

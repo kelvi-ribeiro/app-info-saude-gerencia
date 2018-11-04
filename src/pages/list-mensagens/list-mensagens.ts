b8d089043bf315c3d0f7bdfb1fd0713715b5dd0a
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-11-04 04:18:21 -0200

    :hammer: Usando DTO para lista de mensagemPage

--- src/pages/list-mensagens/list-mensagens.ts
+++ src/pages/list-mensagens/list-mensagens.ts
@@ -38,36 +38,11 @@ export class ListMensagensPage {
   findAllMensagens(){
     this.mensagemService.findAllPageable(this.pageAtual)
     .then(res=>{            
-      res.content.forEach(element => {        
-        if(element.paciente){
-          element.totalMensagemEnviado = 1         
-        }else{
-          this.pacienteService
-        .showNumbersPacienteByLinhaCuidado(element.linhaCuidado ? element.linhaCuidado.id : 0)
-        .then(res=>{          
-          element.totalMensagemEnviado = res;          
-          })          
-        }
-        this.intercaoService.showNumberMessageRed(element.id)
-        .then(numberOfMessageRead =>{
-            element.numberOfMessageRead = numberOfMessageRead              
-          })
-        
-        this.mensagens = res.content       
-      });
-      //this.removerPacienteDuplicado();
-      
-      /* this.pacientes = this.pacientes.sort(function (a, b) {
-        var textA = a.pessoa.nome.toUpperCase(); // ORDENANDO POR NOME
-        var textB = b.pessoa.nome.toUpperCase();
-      
-        return textA.localeCompare(textB);
-      }); */
-      this.totalPages = res.totalPages   
       if(res.content.length === 0){
-        //this.notificacoesService.presentToast('Nenhum paciente encontrado','toast-attention',2000,'top')
+        this.notificacoesService.presentToast('Nenhuma mensagem encontrada','toast-attention',2000,'top')
         return        
       }
+      this.mensagens = res.content;       
       this.pages =  Array(res.totalPages).fill(res.totalPages).map((x,i)=>  i) /* Array(res.totalPages).fill(res.totalPages).map((x,i)=>i); // [0,1,2,3,4] */
       this.pages = this.pages.filter(res => res<=this.pageAtual+5 && res>=this.pageAtual-5)
     }).catch((error)=>{
@@ -96,10 +71,10 @@ export class ListMensagensPage {
     this.findAllMensagens()
   }
   exibirDestinatario(mensagem){
-    if(mensagem.linhaCuidado){
-      return `Linha de cuiado: ${mensagem.linhaCuidado.nome}`
-    }else if(mensagem.paciente){
-      return `Paciente: ${mensagem.paciente.pessoa.nome}`
+    if(mensagem.linhaCuidadoId){
+      return `Linha de cuiado: ${mensagem.nomeLinhaCuidado}`
+    }else if(mensagem.pacienteId){
+      return `Paciente: ${mensagem.nomePaciente}`
     }else{
       return 'Todos'
     }
